9ba91ea959aa602911b9ba7f487dc58df752db85
Joshua Gonzales <jgonzales.ibarrola@gmail.com>
2025-01-07 20:57:37 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <main>
+      <h1 className='text-4xl'>WOW TEXT 4XL!</h1>
+      <Button variant={'secondary'}>WOW TAILWIND!</Button>
+    </main>
+  )
+}
+
+export default App
